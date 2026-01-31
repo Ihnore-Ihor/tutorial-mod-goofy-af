0843128a9124b6f31a03fe6e3b07cef9ea15aaa4
ihnore-ihor <igorbrajchenko@gmail.com>
2026-01-31 17:53:41 +0200

    initial commit

---
+++ src/main/java/net/ihnore_ihor/tutorialmod/TutorialModClient.java
@@ -0,0 +1,11 @@
+package net.ihnore_ihor.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
