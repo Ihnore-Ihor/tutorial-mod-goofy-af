0f9834d0b37e9174351d16e45b9e19c703c12aa4
ihnore-ihor <igorbrajchenko@gmail.com>
2026-01-31 19:35:21 +0200

    add first items

--- src/main/java/net/ihnore_ihor/tutorialmod/TutorialModClient.java
+++ src/main/java/net/ihnore_ihor/tutorialmod/TutorialModClient.java
@@ -1,11 +1,12 @@
 package net.ihnore_ihor.tutorialmod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.ihnore_ihor.tutorialmod.item.ModItems;
 
 public class TutorialModClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
+        ModItems.registerModItems();
     }
 }
